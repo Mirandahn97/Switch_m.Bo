94d3b9aabaee0339b53c554f85a24b51a06c3e8c
Mirandahn97 <mira0249@elev.techcollege.dk>
2025-09-03 13:18:50 +0200

    Opgave 1

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -11,20 +11,53 @@ let myAnsverFeedbackElement = document.getElementById('answerFeedback');
 myAnsverButton.addEventListener('click', (event) => {
     event.preventDefault();
     console.warn('opgave 1 og 2');
+
     /* din kode her.*/
+    const userInput = parseInt(myAnsverInput.value);
+
+    switch (userInput) {
+        case 1:
+            showMessage("Du har valgt 1");
+            break;
+
+        case 2:
+            showMessage("Du har valgt 2");
+            break;
+        case 3:
+            showMessage("Du har valgt 3");
+            break;
+
+        case 4:
+            showMessage("Du har valgt 4");
+            break;
+
+        case 5:
+            showMessage("Du har valgt 5");
+            break;
+
+        default:
+            showMessage("Du skal vælge et tal mellem 1 og 5");
+            break;
+    }
+    //view function
+    function showMessage(myMessage) {
+        myAnsverFeedbackElement.innerHTML = `<p>${myMessage}</p>`;
+    }
+    console.log(userInput)
 });
+
 
 /* skriv evt en view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
 
 
 
 // opgave 3 og 4
- myAnsverInput = document.getElementById('answerTwo');
- myAnsverButton = document.getElementById('answerButtonTwo');
+myAnsverInput = document.getElementById('answerTwo');
+myAnsverButton = document.getElementById('answerButtonTwo');
 
- myAnsverFeedbackElement = document.getElementById('answerFeedbackTwo');
+myAnsverFeedbackElement = document.getElementById('answerFeedbackTwo');
 
- myAnsverButton.addEventListener('click', (event) => {
+myAnsverButton.addEventListener('click', (event) => {
     event.preventDefault();
     console.warn('opgave 3 og 4');
     /* din kode her.*/
@@ -40,16 +73,16 @@ myAnsverButton = document.getElementById('answerButtonFive');
 myAnsverFeedbackElement = document.getElementById('answerFeedbackFive');
 
 myAnsverButton.addEventListener('click', (event) => {
-   event.preventDefault();
+    event.preventDefault();
 
-   console.warn('opgave 5');
+    console.warn('opgave 5');
 
-   workDays(myAnsverInput.value)
-  
+    workDays(myAnsverInput.value)
+
 });
 
 
- /* din kode her.*/
+/* din kode her.*/
 
 
 
